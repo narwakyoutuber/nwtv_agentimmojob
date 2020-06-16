40f5d0c87b61f78166f1306b5ce876489e0571b0
narwakyoutuber <67028935+narwakyoutuber@users.noreply.github.com>
2020-06-17 01:33:39 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,28 @@
+Config                            = {}
+
+Config.DrawDistance               = 100.0
+
+Config.MaxInService               = -1
+Config.EnablePlayerManagement     = true
+
+Config.Locale = 'fr'
+
+Config.Zones = {
+
+	realestateagentActions = {
+		Pos   = { x = -124.786, y = -641.486, z = 168.820 },
+		Size  = {x = 1.0, y = 1.0, z = 1.0},
+		Color = {r = 204, g = 0, b = 0},
+		Type  = 20, Rotate = true,
+		Duty  = 0
+	},
+
+	Cloakroom = {
+		Pos     = {x = -124.45, y = -644.12, z = 168.82},
+		Size    = {x = 1.0, y = 1.0, z = 1.0},
+		Color   = {r = 204, g = 0, b = 0},
+		Type    = 21, Rotate = true,
+		Duty  = 1
+	}
+
+}
